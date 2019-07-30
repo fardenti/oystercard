62b68c32c2c8f35a839fd966e0f49c591818b0f2
monmon8 <student@Makerss-MacBook-Pro.local>
2019-07-30 11:46:41 +0100

    challenge#4 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,7 @@
 class Oystercard
+    attr_reader :balance
+    def initialize 
+        @balance = 0
+    end 
+     
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require "oystercard"
+
 describe Oystercard do
-  it 'a freshly initialized card has a balance of 0' do
+  it 'has a balance of 0' do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
   end
